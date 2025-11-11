ff569bb976f1d4dfc19750f52bc88b53a4e18ac3
Anh Nguyen <anh.nguyenkim@asnet.com.vn>
2025-11-11 10:16:36 +0700

    cors-1

--- test_project/settings.py
+++ test_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -50,7 +51,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "test_project.urls"
@@ -167,7 +167,8 @@ CELERY_TASK_TIME_LIMIT = 30 * 60  # 30 minutes
 
 # CORS Configuration
 CORS_ALLOWED_ORIGINS = [
-    "https://icy-bush-026a07d1e.3.azurestaticapps.net",
+    "*"
+    # "https://icy-bush-026a07d1e.3.azurestaticapps.net",
 ]
 
 # Option 2: Allow all origins (ONLY for development/testing)
