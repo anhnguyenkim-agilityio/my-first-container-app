51fd20f1c60dd2b0a7cfae89ea107f26aa07e425
Anh Nguyen <anh.nguyenkim@asnet.com.vn>
2025-11-11 14:21:47 +0700

    static-2

--- test_project/settings.py
+++ test_project/settings.py
@@ -198,8 +198,7 @@ elif STATIC_ENV == "azure":
     DEFAULT_FILE_STORAGE = "storages.backends.azure_storage.AzureStorage"
     AZURE_ACCOUNT_NAME = os.getenv("AZURE_STORAGE_ACCOUNT")
     AZURE_ACCOUNT_KEY = os.getenv("AZURE_STORAGE_KEY")
-    AZURE_CONTAINER = os.getenv("AZURE_STORAGE_CONTAINER")
-    AZURE_STATIC_CONTAINER = "static"
+    AZURE_CONTAINER = os.getenv("AZURE_STORAGE_CONTAINER", "static")
 
     STATIC_URL = (
         f"https://{AZURE_ACCOUNT_NAME}.blob.core.windows.net/{AZURE_CONTAINER}/"
